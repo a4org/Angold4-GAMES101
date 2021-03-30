1d4ee448c31ffe58cad8a8274a710e4195fb182b
Angold-4 <wangold4w@gmail.com>
2021-03-31 01:56:58 +0800

    finish assigment4

--- Assignment/Assignment2/Rasterizer/rasterizer.cpp
+++ Assignment/Assignment2/Rasterizer/rasterizer.cpp
@@ -206,6 +206,7 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t) {
                     float gamma;
                     std::tie(alpha, beta, gamma) = tup;
                     float w_reciprocal = 1.0 / (alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
+                    // interpolated z
                     float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
                     z_interpolated *= w_reciprocal;
 
