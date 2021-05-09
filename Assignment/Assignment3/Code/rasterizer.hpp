cd316f7c2f2add01e7ebff343106c705358c10ff
AngoldWange <s1239587@ouhk.edu.hk>
2021-05-10 01:30:57 +0800

    This is a Copy for Angold4-GAMES101 with author Jiawei Wang

--- Assignment/Assignment3/Code/rasterizer.hpp
+++ Assignment/Assignment3/Code/rasterizer.hpp
@@ -103,7 +103,7 @@ namespace rst
         std::map<int, std::vector<Eigen::Vector3f>> col_buf;
         std::map<int, std::vector<Eigen::Vector3f>> nor_buf;
 
-        std::optional<Texture> texture;
+        std::optinal<Texture> texture;
 
         std::function<Eigen::Vector3f(fragment_shader_payload)> fragment_shader;
         std::function<Eigen::Vector3f(vertex_shader_payload)> vertex_shader;
---
+++ Assignment/Assignment5/Code/build/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Angold4/WorkSpace/Angold4-GAMES101/Assignment/Assignment5/Code/Renderer.cpp" "/Users/Angold4/WorkSpace/Angold4-GAMES101/Assignment/Assignment5/Code/build/CMakeFiles/RayTracing.dir/Renderer.cpp.o"
+  "/Users/Angold4/WorkSpace/Angold4-GAMES101/Assignment/Assignment5/Code/Scene.cpp" "/Users/Angold4/WorkSpace/Angold4-GAMES101/Assignment/Assignment5/Code/build/CMakeFiles/RayTracing.dir/Scene.cpp.o"
+  "/Users/Angold4/WorkSpace/Angold4-GAMES101/Assignment/Assignment5/Code/main.cpp" "/Users/Angold4/WorkSpace/Angold4-GAMES101/Assignment/Assignment5/Code/build/CMakeFiles/RayTracing.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
